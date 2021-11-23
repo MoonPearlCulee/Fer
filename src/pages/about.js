79cc064916bb2d6424552c1a71c6b3e2a46024d1
Fer Gadea <Luis.fernando.gadea.sanchez@gmail.com>
2021-11-22 22:01:49 -0600

    Title

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import Layout from './layout.js'
-
+import { StaticImage } from "gatsby-plugin-image"
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>El acerca de con todos mis datos.
         Me llamo Fernando y vivo en Morelos uwuwuwu
       </p>
+      <StaticImage
+      src= "C:/Veres/Veres.jpg"
+      />
     </Layout>
   )
 }
