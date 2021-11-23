4ae507178d4d271a6864100cd51f33dbe8f7e499
Fer Gadea <Luis.fernando.gadea.sanchez@gmail.com>
2021-11-22 21:41:53 -0600

    Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "MiPrimeraPagina",
+    title: "Mi Primera Pagina con Query y GraphiQl",
   },
   plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp",],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
       alt="Un cacahuadolar valido en cualquier cacahuatienda participante uwu"
       src="https://i.pinimg.com/564x/ec/66/96/ec66964c084324cfcb3266686d4efd2d.jpg"
       />
-      <img
+      <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://i.pinimg.com/736x/67/07/ef/6707ef2927af272b414c07f963a015d3.jpg"
         width = "500"
