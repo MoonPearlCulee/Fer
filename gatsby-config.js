81c2a4e4a8c43582326f5dae7a8ffd5cc9b35bdc
Fer Gadea <Luis.fernando.gadea.sanchez@gmail.com>
2021-11-22 23:22:27 -0600

    Proyecto

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Mi Primera Pagina con Query y GraphiQl",
+    title: "Tienda de musica u.u",
   },
   plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image", "gatsby-plugin-sharp",],
 };
