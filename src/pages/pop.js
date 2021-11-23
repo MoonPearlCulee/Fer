ed1109b2cbb0bcb9b8cc9cbda68c0cada3b416df
Fer Gadea <Luis.fernando.gadea.sanchez@gmail.com>
2021-11-23 00:34:15 -0600

    Musica

--- src/pages/pop.js
+++ src/pages/pop.js
@@ -12,7 +12,9 @@ const PopPage = () => {
       src="https://scontent-qro1-2.xx.fbcdn.net/v/t1.6435-9/118227896_2047109205421044_3663583107540423347_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=9blWn41OvfIAX8aCzW2&_nc_ht=scontent-qro1-2.xx&oh=a4e20af687b457f34aa080dffd12b8aa&oe=61C0D8B2"
       />
       <p>Enter</p>
-      <audio src="../music/cowboylikeme.mp3" controls></audio>
+      <audio source="../music/cowboylikeme.mp3" controls>
+          <p>Esta canción es Cowboy like me</p>
+      </audio>
     </Layout>
   )
 }
